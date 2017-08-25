0c372ded74efaaba519fa30eec31a88932675c51
kvnmlr <kevin.mueller1@live.de>
2017-08-25 04:40:37 +0200

    making an app interactive part 1

--- user-input/app/src/main/java/com/example/android/justjava/MainActivity.kt
+++ user-input/app/src/main/java/com/example/android/justjava/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.android.justjava
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun submitOrder(view: View) {
+        var quantity = 2;
+        display(quantity)
+        displayPrice(quantity * 2);
+    }
+
+    private fun display(number: Int) {
+        val quantityTextView = findViewById<TextView>(R.id.quantity_text_view) as TextView
+        quantityTextView.text = number.toString()
+    }
+
+    private fun displayPrice(number: Int) {
+        val priceTextView = findViewById<View>(R.id.price_text_view) as TextView
+        priceTextView.text = NumberFormat.getCurrencyInstance().format(number)
     }
 }

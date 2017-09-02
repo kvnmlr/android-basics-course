4dd894851a3e01cb4d0de2f6b080b7a835d3eac2
kvnmlr <kevin.mueller1@live.de>
2017-09-02 14:43:57 +0200

    display words in numbersactivity

--- multiscreen-apps/miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.kt
+++ multiscreen-apps/miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.kt
@@ -2,11 +2,33 @@ package com.example.android.miwok
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
 
-class NumbersActivity() : AppCompatActivity() {
+class NumbersActivity : AppCompatActivity() {
+    private var wordsList: ArrayList<String> = ArrayList()
+    private var wordsArray: Array<String> = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+    private lateinit var words: Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numbers)
+        var rootView = findViewById(R.id.root_view_numbers) as LinearLayout
+
+        words = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+
+        for ((index, value) in words.withIndex()) {
+            wordsList.add(value)
+            Log.v("NumbersActivity", "$index: $value")
+            Log.v("NumbersActivity", "Word at index $index: ${wordsList[index]}")
+            Log.v("NumbersActivity", "Word at index $index: ${wordsArray[index]}")
+            Log.v("NumbersActivity", "Word at index $index: $value")
+            assert(wordsList[index] == wordsArray[index] && wordsList[index] == value)
+
+            var wordView = TextView(this)
+            wordView.text = words[index]
+            rootView.addView(wordView)
+        }
     }
 }
